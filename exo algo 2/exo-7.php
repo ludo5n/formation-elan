cde2ddaf24e26b789f0d8e089056e3fa73617963
ludo5n <ludo5n@gmail.com>
2020-03-18 17:27:30 +0100

    changement exo7

--- exo algo 2/exo-7.php
+++ exo algo 2/exo-7.php
@@ -1,11 +1,11 @@
-<?php $elements = array("choix1" => "", "choix2" => "", "choix3" => "checked");
+<?php 
+
+$elements = ["choix1" => "", "choix2" => "", "choix3" => "checked"];
+
 function genererCheckbox($elements){
-    // echo "<form>";
-foreach ($elements as $key => $value){
-     echo "<input type='checkbox'name='$key'> $key</input><label for='$key'>$value</label><br/>";
-     
-    } ;
-    // echo "</form>";
-}
+   foreach ($elements as $key => $value){
+     echo "<input type='checkbox'name=''> $key  </input><label for='$key'>$value</label><br/>";
+         } 
+   }
 genererCheckbox($elements);
 ?>
