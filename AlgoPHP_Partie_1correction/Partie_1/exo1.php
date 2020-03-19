a44c9abedb889241f111b471c4473e051e417b16
ludo5n <ludo5n@gmail.com>
2020-03-19 20:44:19 +0100

    mise a jour

---
+++ AlgoPHP_Partie_1correction/Partie_1/exo1.php
@@ -0,0 +1,6 @@
+<?php
+
+$phrase = "Notre formation DL commence aujourd'hui";
+$longueur = strlen($phrase); // strlen() compte le nombre de caractères
+echo "La phrase « $phrase » contient ".strlen($phrase)." caractères";
+echo "La phrase « $phrase » contient $longueur caractères";
