c7a9c58a480877885e68b772a9fdf7ede25afcc3
ludo5n <ludo5n@gmail.com>
2020-03-28 20:47:12 +0100

    maj

--- exo algo 2/exo-13.php
+++ exo algo 2/exo-13.php
@@ -4,12 +4,27 @@ class Voiture {
     private $modele;
     private $nbPortes;
     private $vitesseActuelle= 0;
+    private $statut = false;
         public function __construct($marque,$modele,$nbPortes){
             $this->marque = $marque;
             $this->modele = $modele;
             $this->nbPortes = $nbPortes;
         }
-    public function getinfo(){
+        public function getMarque(){
+            return $this->marque;
+        }
+        public function getModele(){
+            return $this->modele;
+        }
+        public function getNbPortes(){
+            return $this->nbPortes;
+        }
+        public function __toString(){
+            return "La voiture " . $this->getMarque()." ".$this->getModele()." possède ".$this->getNbPortes(). " portes";
+        }
+    
+    
+        public function getinfo(){
         $info=[];
         $i=0;
         for ($marque=0;$marque<sizeof($info);$marque++){
@@ -19,39 +34,41 @@ class Voiture {
         }  
         echo "info".$voiture.$i."<br>";
         echo"nom et modele du véhicule".$this->marque."".$this->model."<br>";
-       echo "Nombre de porte".$this->nbPortes;
-        if (getdemarrer==true){
+        echo "Nombre de porte".$this->nbPortes;
+        if ($this->statut){
             $voitinfo="le vehicule est démarré ,sa vitesse actuelle est de ".$this->getvitesseActuelle."km";
         
-        }return $voitinfo;
+        }
+        return $voitinfo;
     }
-    public function getdemarrer()
+     public function getdemarrer()
     {
-            return "le véhicule".$this->marque."".$this->modele."démarre"; 
+        return $statut = true;
     }
-    public function ralentir()
+        public function ralentir($valeur)
     {
-        $ralenti= getvitesseActuelle() - ralentir();
+        $ralenti= getvitesseActuelle() - ($value);
         $ralenti=$this->marque.$this->modele."ralenti de ".ralentir()."km"; 
         return $ralenti;
     }
     public function getstopper()
     {
-            return "Le véhicule".$this->marque."".$this->modele."est stoppé";
+        return "Le véhicule".$this->marque."".$this->modele."est stoppé";
     }
-    public function setaccelerer()
+    public function setaccelerer($value)
     {
-        if(getdemarrer== true)
+        if($this->getdemarrer)
             {
-                  echo"le vehicule".$this->marque."".$this->modele."veut accelerer de".setaccelerer()."km,"."<br>
+                $this->setaccelerer += $value;
+                  echo"le vehicule".$this->marque."".$this->modele."veut accelerer de".$value."km,"."<br>
                   Pour accelerer,ilfaudrait deja démarrer le vehicule".$this->marque.$this->modele."!";
              }else{
-        return "Le véhicule".$this->marque."".$this->modele." accélere de ".setaccelerer()."km";
+        return "Le véhicule".$this->marque."".$this->modele." accélere de ".$value."km";
             }
     }
     public function getvitesseActuelle(){
        
-        if (getdemarrer ==true)
+        if ($this->getdemarrer)
         {
              $vitesseActuelle =$this->setaccelerer()+$this->setaccelere();
       echo   $vitessseActuelle="La vitesse actuelle du vehicule est de ".$this->$vitesseActuelle."km.";
@@ -61,7 +78,9 @@ class Voiture {
           
     }
 }
- $v1=new Voiture ("peugeot", "408",5);
- $v2=new Voiture ("Citroen","c4",3);
- $v1->getdemarrer();
+$v1 = new Voiture("Peugeot","508",5)."<br>";
+$v2 = new Voiture("Citrôen","C4",5);
+echo $v1; 
+echo $v2; 
+ $v1-> getdemarrer();
 ?>
